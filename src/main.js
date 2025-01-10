f3ed607671655ce7d12c9e0a3be5ed06a1705c71
Willian Sant Anna <w.s.santanna@gmail.com>
2025-01-10 00:21:47 -0300

    injeção dos principais componentes do boostrap

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
 import "./scss/custom-bootstrap.scss"
+import {
+    Alert,
+    Button,
+    Carousel,
+    Collapse,
+    Modal,
+    Tooltip,
+    Popover,
+    ScrollSpy,
+    Toast,
+    Dropdown,
+    Offcanvas
+} from "bootstrap"
